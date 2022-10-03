5f066f1113699146443f08f39c06a0a90f0c12df
Mohan Ganesh Gollapalli <mohanganeshgollapalli@gmail.com>
2022-10-03 22:46:13 +0530

    tricky questions file created

---
+++ tricky_questions.js
@@ -0,0 +1,22 @@
+// will it print hello medium
+let a = {
+    name: 'mohan',
+    toString() {
+        return 'medium';
+    }
+}
+
+let person = {
+    user: "MOhan"
+}
+
+console.log(person.toString())
+console.log(a);
+
+if (a == 'medium') {
+    console.log('hello medium');
+}
+
+// ----------------------------------------------
+
+
